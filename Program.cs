f49153a76e820b4558976a63189f680e81cf30b6
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-05-27 16:41:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        // Enum for the days of the week
+        enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Prompt the user to enter the current day of the week
+                Console.WriteLine("Enter the current day of the week:");
+
+                // Read the user input
+                string userInput = Console.ReadLine();
+
+                // Parse the user input to the enum type
+                DaysOfWeek currentDay;
+                bool isValidDay = Enum.TryParse(userInput, out currentDay);
+
+                if (isValidDay)
+                {
+                    // Assign the value to a variable of the enum data type
+                    Console.WriteLine("Current day: " + currentDay);
+                }
+                else
+                {
+                    // Handle invalid input
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
